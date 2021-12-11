730d70cb1e121171fba84f6fdaff4142939e0e7e
Jhon salazar <jhonzallazar@gmail.com>
2021-12-11 00:08:25 -0500

    Se creo el respectivo controller,repositorie y service para la creacion del end point

---
+++ src/main/java/co/com/sofka/calendar/controllers/ShedulerController.java
@@ -0,0 +1,40 @@
+package co.com.sofka.calendar.controllers;
+
+
+import co.com.sofka.calendar.collections.Program;
+import co.com.sofka.calendar.model.ProgramDate;
+import co.com.sofka.calendar.services.ProgramDateService;
+import co.com.sofka.calendar.services.SchedulerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@CrossOrigin("*") @RequestMapping("/api")
+public class ShedulerController {
+
+    @Autowired
+    SchedulerService schedulerService;
+
+    @Autowired
+    ProgramDateService programDateService;
+
+
+    @GetMapping("/generate/{date}/{id}")
+    public Flux<ProgramDate> generateCalendar(@PathVariable("date") String date, @PathVariable("id") String id) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        //convert String to LocalDate
+        LocalDate localDate = LocalDate.parse(date, formatter);
+        Flux<ProgramDate> generado = schedulerService.generateCalendar(id, localDate);
+        return programDateService.saveAll(generado);
+    }
+
+    @GetMapping("/programas")
+    public Flux<ProgramDate> programas() {
+        return programDateService.getAll();
+    }
+
+}
